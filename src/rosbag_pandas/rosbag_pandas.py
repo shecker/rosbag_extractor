50e6efca218e3c93d6a56aa5a8047e82d55dfdac
NicoZobernig <nico.zobernig@gmail.com>
2021-03-23 10:48:43 +0100

    found code again

--- src/rosbag_pandas/rosbag_pandas.py
+++ src/rosbag_pandas/rosbag_pandas.py
@@ -7,6 +7,12 @@ import numpy as np
 import pandas as pd
 import rosbag
 from rospy_message_converter.message_converter import convert_ros_message_to_dictionary
+from scipy.interpolate import interp1d
+# from sensor_msgs.msg import Image, CompressedImage
+# from sensor_msgs.msg import CameraInfo
+# from cv_bridge import CvBridge, CvBridgeError
+import cv2
+import os
 
 
 class RosbagPandaException(Exception):
@@ -29,7 +35,7 @@ def topics_from_keys(keys):
     return list(topics)
 
 
-def bag_to_dataframe(bag_name, include=None, exclude=None):
+def bag_to_dataframe(bag_name, include=None, exclude=None, output=None):
     """
     Read in a rosbag file and create a pandas data frame that
     is indexed by the time the message was recorded in the bag.
@@ -46,8 +52,13 @@ def bag_to_dataframe(bag_name, include=None, exclude=None):
     type_topic_info = bag.get_type_and_topic_info()
     topics = type_topic_info.topics.keys()
 
-    print("topics: ", topics)
-    return
+    output_images = os.path.join(output, '/images/')
+
+    try:
+        os.makedirs(output_images)
+        print("Directory '%s' created" % output_images)
+    except FileExistsError as e:
+        print("Directory '%s' already exists" % output_images)
 
     # get list of topics to parse
     logging.debug("Bag topics: %s", topics)
@@ -61,28 +72,76 @@ def bag_to_dataframe(bag_name, include=None, exclude=None):
     if not topics:
         raise RosbagPandaException("No topics in bag after filtering")
 
-    df_length = sum([type_topic_info.topics[t].message_count for t in topics])
+    img_idx = {}
+    for topic in topics:
+        if "image" in topic:
+            image_folder = output_images + topic.split('/')[1]
+            if topic not in img_idx:
+                img_idx[topic] = {'folder': image_folder, 'counter': 0}
+            try:
+                os.makedirs(os.path.join(output_images, image_folder))
+                print("Directory '%s' created" % output)
+            except FileExistsError as e:
+                print("Directory '%s' already exists" % output)
 
-    index = np.empty(df_length)
-    index.fill(np.NAN)
     data_dict = {}
     for idx, (topic, msg, t) in enumerate(bag.read_messages(topics=topics)):
         flattened_dict = _get_flattened_dictionary_from_ros_msg(msg)
+        timestamp = float(str(flattened_dict['header/stamp/secs']) + '.' + str(flattened_dict['header/stamp/nsecs']))
+
         for key, item in flattened_dict.items():
+            if ('header' in key) or ('camera_info' in topic) or ('format' in key):
+                continue
+            elif 'image' in topic:
+                np_arr = np.fromstring(msg.data, np.uint8)
+                # image_np = cv2.imdecode(np_arr, cv2.CV_LOAD_IMAGE_COLOR)
+                image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)  # OpenCV >= 3.0:
+                image_path = img_idx[topic]['folder'] + '/' + str(img_idx[topic]['counter']) + '.jpg'
+                if not image_np:
+                    continue
+                cv2.imwrite(image_path, image_np)
+                img_idx[topic]['counter'] += 1
+                # cv2.imshow('cv_img', image_np)
+                # cv2.waitKey(0)
+                item = image_path.split('images')[1]
+
             data_key = topic + "/" + key
             if data_key not in data_dict:
-                if isinstance(item, float) or isinstance(item, int):
-                    data_dict[data_key] = np.empty(df_length)
-                    data_dict[data_key].fill(np.NAN)
-                else:
-                    data_dict[data_key] = np.empty(df_length, dtype=np.object)
-            data_dict[data_key][idx] = item
-        index[idx] = t.to_sec()
+                data_dict[data_key] = {'ts': [],
+                                       'data': []}
+
+            data_dict[data_key]['ts'].append(timestamp)
+            data_dict[data_key]['data'].append(item)
+
+    df = {'timestamp': data_dict['/zed2/zed_node/left/image_rect_color/compressed/data']['ts'],
+          'zed2': data_dict['/zed2/zed_node/left/image_rect_color/compressed/data']['data']}
+    for k, v in data_dict.items():
+        if 'zed2' in k:
+            continue
+        if 'eye' in k:
+            indices = [i for i in range(len(data_dict[k]['data']))]
+            interpolation_function = interp1d(data_dict[k]['ts'],
+                                              indices,
+                                              bounds_error=False,
+                                              fill_value='extrapolate',
+                                              kind='nearest')
+        else:
+            interpolation_function = interp1d(data_dict[k]['ts'],
+                                              data_dict[k]['data'],
+                                              bounds_error=False,
+                                              fill_value='extrapolate',
+                                              kind='linear')
+        if 'eye' in k:
+            indices = interpolation_function(data_dict['/zed2/zed_node/left/image_rect_color/compressed/data']['ts'])
+            df[k] = [data_dict[k]['data'][int(i)] for i in indices]
+        else:
+            df[k] = interpolation_function(data_dict['/zed2/zed_node/left/image_rect_color/compressed/data']['ts'])
 
     bag.close()
 
     # now we have read all of the messages its time to assemble the dataframe
-    return pd.DataFrame(data=data_dict, index=index)
+    df_out = pd.DataFrame(data=df)
+    df_out.to_csv(output + '/data.csv')
 
 
 def _get_flattened_dictionary_from_ros_msg(msg):
